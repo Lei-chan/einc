5adc93bd6123d83bdd1d473240abf970f2831aa3
Lei-chan <hirosebirosepirose01@yahoo.co.jp>
2026-01-09 21:57:08 +0900

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,22 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div className="w-screen h-screen">
+      <div className="relative w-full h-[14%] bg-gradient-to-r from-yellow-200 to-orange-300 flex flex-row items-center">
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          src="/einc-logo.PNG"
+          alt="einc logo"
+          width={500}
+          height={250}
+          className="w-auto h-full aspect-[1/0.5] object-cover"
         />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+        <div className="absolute w-[60%] h-fit flex flex-row text-base gap-3 right-4 text-red-600 text-center items-center leading-tight justify-end">
+          <button className="bg-[url('/icons/language.svg')] w-[15%] aspect-square bg-center bg-contain bg-no-repeat"></button>
+          <p>Login</p>
+          <p>Sign up</p>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
