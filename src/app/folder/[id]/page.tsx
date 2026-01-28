2e7a800b5fad77986a8ce10682e49e73c59e9c98
Lei-chan <hirosebirosepirose01@yahoo.co.jp>
2026-01-28 21:39:19 +0900

    Updated

--- src/app/folder/[id]/page.tsx
+++ src/app/folder/[id]/page.tsx
@@ -1,8 +1,30 @@
+"use client";
 import LinkAddVocab from "@/app/Components/LinkAddVocab";
 import Logo from "@/app/Components/Logo";
+//libraries
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  ArcElement,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  plugins,
+} from "chart.js";
+import { Line, Pie } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
+import { use } from "react";
+import { time } from "console";
 
-export default async function Folder(props: PageProps<"/folder/[id]">) {
-  const { id } = await props.params;
+export default function Folder({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = use(params);
 
   return (
     <div className="w-screen h-fit flex flex-col items-center gap-12">
@@ -43,10 +65,90 @@ function ButtonContent({ name }: { name: string }) {
   );
 }
 
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
 function Graph() {
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    ArcElement,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  );
+
   return (
-    <div className="w-full h-60 bg-gradient-to-tl from-red-600/70 to-red-400/70 p-2">
-      <h2 className="text-xl font-medium text-white">Your Progress</h2>
+    <div className="w-full h-fit bg-gradient-to-tl from-red-600/50 to-red-400/50 px-3 py-5">
+      <h2 className="text-xl">Your Progress</h2>
+      <div className="relative w-full h-fit bg-white rounded flex flex-col gap-8 mt-5">
+        <Pie
+          options={{
+            plugins: {
+              title: {
+                display: true,
+                text: "Today's Progress",
+                color: "brown",
+              },
+            },
+          }}
+          data={{
+            labels: [
+              "Completed",
+              "Mostly completed",
+              "A little difficult",
+              "Difficult",
+            ],
+            datasets: [
+              {
+                label: "Number of words",
+                data: [12, 19, 3, 5],
+                backgroundColor: [
+                  "rgba(255, 99, 132, 0.9)",
+                  "rgba(54, 162, 235, 0.9)",
+                  "rgba(255, 206, 86, 0.9)",
+                  "rgba(75, 192, 192, 0.9)",
+                ],
+              },
+            ],
+          }}
+        />
+        <Line
+          options={{
+            plugins: {
+              title: { display: true, text: "Recent Progress", color: "brown" },
+              legend: {
+                position: "top" as const,
+              },
+            },
+            responsive: true,
+            maintainAspectRatio: true,
+            aspectRatio: 1.3 / 1,
+          }}
+          data={{
+            labels,
+            datasets: [
+              {
+                label: "Completed",
+                data: labels.map(() =>
+                  faker.number.int({ min: -1000, max: 1000 }),
+                ),
+                borderColor: "rgb(255, 99, 132)",
+                backgroundColor: "rgba(255, 99, 132, 0.5)",
+              },
+              {
+                label: "Difficult",
+                data: labels.map(() =>
+                  faker.number.int({ min: -1000, max: 1000 }),
+                ),
+                borderColor: "rgb(53, 162, 235)",
+                backgroundColor: "rgba(53, 162, 235, 0.5)",
+              },
+            ],
+          }}
+        />
+      </div>
     </div>
   );
 }
