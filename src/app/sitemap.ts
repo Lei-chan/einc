3e98e6848f664cb3b1b5dada016a04829bed2a44
Lei-chan <hirosebirosepirose01@yahoo.co.jp>
2026-03-18 21:12:47 +0900

    Updated

---
+++ src/app/sitemap.ts
@@ -0,0 +1,115 @@
+import type { MetadataRoute } from "next";
+import { BASE_URL } from "./lib/config/settings";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: BASE_URL,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+      alternates: {
+        languages: {
+          en: `${BASE_URL}en`,
+          ja: `${BASE_URL}ja`,
+        },
+      },
+    },
+    {
+      url: `${BASE_URL}account`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.4,
+      alternates: {
+        languages: {
+          en: `${BASE_URL}en/account`,
+          ja: `${BASE_URL}ja/account`,
+        },
+      },
+    },
+    {
+      url: `${BASE_URL}account-closed`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.1,
+      alternates: {
+        languages: {
+          en: `${BASE_URL}en/account-closed`,
+          ja: `${BASE_URL}ja/account-closed`,
+        },
+      },
+    },
+    {
+      url: `${BASE_URL}add`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.4,
+      alternates: {
+        languages: {
+          en: `${BASE_URL}en/add`,
+          ja: `${BASE_URL}ja/add`,
+        },
+      },
+    },
+    {
+      url: `${BASE_URL}add-to`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.1,
+      alternates: {
+        languages: {
+          en: `${BASE_URL}en/add-to`,
+          ja: `${BASE_URL}ja/add-to`,
+        },
+      },
+    },
+    {
+      url: `${BASE_URL}collection`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.4,
+      alternates: {
+        languages: {
+          en: `${BASE_URL}en/collection`,
+          ja: `${BASE_URL}ja/collection`,
+        },
+      },
+    },
+    {
+      url: `${BASE_URL}dictionary`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.4,
+      alternates: {
+        languages: {
+          en: `${BASE_URL}en/dictionary`,
+          ja: `${BASE_URL}ja/dictionary`,
+        },
+      },
+    },
+    {
+      url: `${BASE_URL}login`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+      alternates: {
+        languages: {
+          en: `${BASE_URL}en/login`,
+          ja: `${BASE_URL}ja/login`,
+        },
+      },
+    },
+    {
+      url: `${BASE_URL}sign-up`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+      alternates: {
+        languages: {
+          en: `${BASE_URL}en/sign-up`,
+          ja: `${BASE_URL}ja/sign-up`,
+        },
+      },
+    },
+  ];
+}
