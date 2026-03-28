55e8a7b412e78b61ee36606d9f6207e7b94138ac
Lei-chan <hirosebirosepirose01@yahoo.co.jp>
2026-03-28 15:21:23 +0900

    Updated sw

--- public/sw.js
+++ public/sw.js
@@ -14,7 +14,6 @@ self.addEventListener("push", function (event) {
       icon: "/icons/icon-192x192.png",
       badge: "/icons/icon-192x192.png",
       url: data.url,
-      vibrate: [100, 50, 100],
       data: {
         dateOfArrival: Date.now(),
         primaryKey: "2",
