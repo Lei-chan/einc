99cbdc0e1827a950d94f2d007bee58cc72f1cf90
Lei-chan <hirosebirosepirose01@yahoo.co.jp>
2026-08-18 17:08:06 -0400

    Updated manifest icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -13,12 +13,12 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: "portrait",
     icons: [
       {
-        src: "/icons/icon-192x192.png",
+        src: "/icons/rounded icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icons/icon-512x512.png",
+        src: "/icons/rounded icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
